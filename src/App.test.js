264353287b0dfd8636bc3576a6650c06d83c2b9f
yohmer <yoh_m@yahoo.fr>
2017-12-24 13:55:09 -0500

    create <App>

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import ordersView from './reducers';
 import App from './App';
+import initOrders from './datas/initOrders';
+import initFilters from './datas/initFilters';
+
+
+const store = createStore(ordersView, {
+  filters: initFilters,
+  orders: initOrders,
+});
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(// eslint-disable-line function-paren-newline
+    <Provider store={store}><App /></Provider>, div);
 });
