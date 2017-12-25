b7ded3003ad8b8c6e64a5f4a95e893af55ab1147
yohmer <yoh_m@yahoo.fr>
2017-12-25 15:47:31 -0500

    Write <ContButtonsMoreLess>

--- src/Containers/ContButtonsMoreLess.js
+++ src/Containers/ContButtonsMoreLess.js
@@ -1,7 +1,29 @@
-import React from 'react';
-// import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import ButtonsMoreLess from '../Presentationals/ButtonsMoreLess';
+import datas from '../datas/datas';
+import { onDone, oneMoreTodo, allDone } from '../actions';
 
+export const mapStateToProps = (state, ownProps) => {
+  const order = state.orders.find(item => (ownProps.orderId.toString() === item.id.toString()));
 
-const ContButtonsMoreLess = () => (<span />);
+  const group = datas.groups[order.groupId];
+  const children = `${datas.lists[group.listId].name} -> ${datas.groups[order.groupId].name} -> ${
+    datas.articles[order.articleId].name}`;
+  return {
+    orderId: order.id,
+    labelOneDone: datas.txt.labelOneDone,
+    labelOneMoreToDo: datas.txt.labelOneMoreToDo,
+    labelAllDone: datas.txt.labelAllDone,
+    children,
+    orderDone: order.done,
+    orderToDo: order.toDo,
+  };
+};
 
-export default ContButtonsMoreLess;
+const mapDispatchToProps = dispatch => ({
+  oneDoneClick: orderId => dispatch(onDone(orderId)),
+  oneMoreTodoClick: orderId => dispatch(oneMoreTodo(orderId)),
+  allDoneClick: orderId => dispatch(allDone(orderId)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(ButtonsMoreLess);
---
+++ src/Containers/ContButtonsMoreLess.test.js
@@ -0,0 +1,19 @@
+import { mapStateToProps } from './ContButtonsMoreLess';
+import initOrders from '../datas/initOrders';
+import datas from '../datas/datas';
+
+const initialState = { orders: initOrders };
+
+describe('ContButtonsMoreLess:', () => {
+  it('should convert the state to props', () => {
+    expect(mapStateToProps(initialState, { orderId: 1 })).toEqual({
+      orderId: '1',
+      labelOneDone: datas.txt.labelOneDone,
+      labelOneMoreToDo: datas.txt.labelOneMoreToDo,
+      labelAllDone: datas.txt.labelAllDone,
+      children: 'boissons -> Apéritifs -> martini rouge',
+      orderDone: 0,
+      orderToDo: 2,
+    });
+  });
+});
